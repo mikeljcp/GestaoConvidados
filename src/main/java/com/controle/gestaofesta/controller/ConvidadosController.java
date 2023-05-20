16aced2d2619d59f4a7b19d219149428f960e901
Mikel J <mikeljcp@gmail.com>
2023-05-20 20:58:24 -0300

    Configurado metodo para salvar em banco de dados.

--- src/main/java/com/controle/gestaofesta/controller/ConvidadosController.java
+++ src/main/java/com/controle/gestaofesta/controller/ConvidadosController.java
@@ -3,8 +3,10 @@ package com.controle.gestaofesta.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.controle.gestaofesta.model.Convidado;
 import com.controle.gestaofesta.repository.Convidados;
 
 @Controller
@@ -20,5 +22,21 @@ public class ConvidadosController {
 		modelAndView.addObject("convidados", convidados.findAll());
 		return modelAndView;
 	}
+	
+	/*@PostMapping("/convidados")
+	public ModelAndView adicionar() {
+		ModelAndView modelAndView = new ModelAndView("ListaConvidados");
+		
+		modelAndView.addObject(new Convidado());
+		return modelAndView;
+	}*/
+	
+	@PostMapping("/convidados")
+	public String salvar(Convidado convidado) {
+		this.convidados.save(convidado);
+		return "redirect:/convidados";
+	}
+	
+
 
 }
