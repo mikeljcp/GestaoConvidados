d3007035b102c7e0d25ebd806808689b7db7eda7
Mikel J <mikeljcp@gmail.com>
2023-05-20 21:00:29 -0300

    Melhoria nos metodos POST e GET

--- src/main/java/com/controle/gestaofesta/controller/ConvidadosController.java
+++ src/main/java/com/controle/gestaofesta/controller/ConvidadosController.java
@@ -4,18 +4,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.controle.gestaofesta.model.Convidado;
 import com.controle.gestaofesta.repository.Convidados;
 
 @Controller
+@RequestMapping("/convidados")
 public class ConvidadosController {
 	
 	@Autowired
 	private Convidados convidados;
 	
-	@GetMapping("/convidados")
+	@GetMapping
 	public ModelAndView listar() {
 		ModelAndView modelAndView = new ModelAndView("ListaConvidados");
 		
@@ -31,7 +33,7 @@ public class ConvidadosController {
 		return modelAndView;
 	}*/
 	
-	@PostMapping("/convidados")
+	@PostMapping
 	public String salvar(Convidado convidado) {
 		this.convidados.save(convidado);
 		return "redirect:/convidados";
